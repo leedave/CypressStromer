46c491ae4bdedd601e4f23431678b7d722c9fdd9
daniviorel <daniel.viorel@arcmedia.ch>
2023-07-19 15:41:48 +0200

    create new project

---
+++ cypress.config.js
@@ -0,0 +1,15 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  projectId: "",
+  viewportWidth: 1920,
+  viewportHeight: 1280,
+  e2e: {
+    setupNodeEvents(on, config) {},
+    baseUrl: "https://www.stromerbike.com/",
+    viewportWidth: 1920,
+    viewportHeight: 1280,
+    specPattern: 'cypress/e2e/tests/**/*.cy.{js,jsx,ts,tsx}',
+  },
+  'defaultCommandTimeout': 10000,
+});
