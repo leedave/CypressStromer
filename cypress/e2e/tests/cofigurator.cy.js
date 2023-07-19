ec236aa27af7090af3982504a2b06987f81d636e
daniviorel <daniel.viorel@arcmedia.ch>
2023-07-19 18:27:46 +0200

    update project

--- cypress/e2e/tests/cofigurator.cy.js
+++ cypress/e2e/tests/cofigurator.cy.js
@@ -1,8 +1,3 @@
-describe('My First Test', () => {
-    it('Does not do much!', () => {
-      expect(true).to.equal(false)
-    })
-  })
   describe('My First Test', () => {
     it('Take a look at our E-bikes', () => {
       cy.visit('https://www.stromerbike.com/')
