b525599d9303f184b5aaaad7c6ab80984c160d98
daniviorel <daniel.viorel@arcmedia.ch>
2023-08-24 17:29:19 +0200

    starting config ST3

---
+++ cypress/e2e/tests/ST3/configuratorDeutsch.cy.js
@@ -0,0 +1,127 @@
+let config = Cypress.config()
+describe('Bike Configurator', function () {
+
+  beforeEach('before each test', function () {
+    cy.visit(`${config.baseUrl}`);
+    cy.get('#popup-buttons')
+      .should('be.visible')
+      .click();
+  });
+
+//Select a language and country
+
+  it('Select language', function ()  {
+    cy.get('#block-multiswitcher-2')
+      .should('be.visible')
+      .click();
+  
+    cy.get('#aos-locale-multi-switcher-form--2 .custom-js-dropdown.custom-js-dropdown-language')
+      .should('be.visible')
+      .click();
+
+    cy.get('#aos-locale-multi-switcher-form--2 .custom-js-dropdown-option.custom-js-dropdown-language-option').contains('Deutsch')
+      .should('be.visible')
+      .click();
+      
+    cy.get('#aos-locale-multi-switcher-form--2 [cdata-selector-target="aos-multi-switcher-country-selector"]')
+      .should('be.visible')
+      .click();
+
+    cy.get('#aos-locale-multi-switcher-form--2 .custom-js-dropdown-option.custom-js-dropdown-country-options').contains('Germany')
+      .should('be.visible')
+      .click();
+
+    cy.get('#edit-submit--2')
+      .should('be.visible')
+      .click();
+  });
+
+//Select a  ST3 E-bike
+
+  it('Your Stromer -> ST3', function () {
+    cy.get('#block-menureferencesbycountry > :nth-child(2) > :nth-child(2) > :nth-child(1)')
+      .should('be.visible')
+      .click();
+    cy.location().should(loc => {
+        expect(loc.pathname).to.equal('/en/configurator-model-select');
+    });
+  
+    cy.get(':nth-child(2) > .paragraph--type--aospcc01 > .container > :nth-child(1) > .col-lg-12 > .grid-container > .row > :nth-child(1) > .str-custom-input-wrapper > .str-custom-input > .str-custom-input-inner > .str-custom-input-content > .aospci-content--links > .field > .field__item > .ghost-link')
+      .trigger('mouseover')
+      .should('be.visible')
+      .click();
+
+//Configure the bike
+// Frame color Cool White 
+// Option 1 -> 
+
+
+cy.get('[for="Farbe_lg"]')
+      .should('be.visible')
+      .click();
+    cy.get('#bikepreviewimg').should('have.prop', 'src', 'https://www.stromerbike.com/bike-configurator/img/bikes/st10lg.jpg');
+
+    cy.get('[for="Rahmenart_sp"]')
+      .should('be.visible')
+      .click();
+    cy.get('#bikepreviewimg').should('have.prop', 'src', 'https://www.stromerbike.com/bike-configurator/img/bikes/st10lg.jpg');
+
+    cy.get('[for="Rahmengrösse_m0"]')
+      .should('be.visible')
+      .click();
+    cy.get('#bikepreviewimg').should('have.prop', 'src', 'https://www.stromerbike.com/bike-configurator/img/bikes/st10lgspm0.jpg');
+
+    cy.get('[for="Federgabel_nf"]')
+      .should('be.visible')
+      .click();
+    cy.get('#bikepreviewimg').should('have.prop', 'src', 'https://www.stromerbike.com/bike-configurator/img/bikes/st10lgspm0.jpg');
+
+    cy.get('[for="Sattelstütze_ns"]')
+      .should('be.visible')
+      .click();
+    cy.get('#bikepreviewimg').should('have.prop', 'src', 'https://www.stromerbike.com/bike-configurator/img/bikes/st10lgspm0.jpg');
+
+    // Battery range
+
+    cy.get('[for="Reichweite_12"]')
+      .should('be.visible')
+      .click();
+    cy.get('#bikepreviewimg').should('have.prop', 'src', 'https://www.stromerbike.com/bike-configurator/img/bikes/st10lgspm0.jpg');
+    cy.get('[for="Reichweite_15"]')
+      .should('be.visible')
+      .click();
+    cy.get('#bikepreviewimg').should('have.prop', 'src', 'https://www.stromerbike.com/bike-configurator/img/bikes/st10lgspm0.jpg');
+    cy.get('[for="Reichweite_18"]')
+      .should('be.visible')
+      .click();
+    cy.get('#bikepreviewimg').should('have.prop', 'src', 'https://www.stromerbike.com/bike-configurator/img/bikes/st10lgspm0.jpg');
+    
+      
+// Testing all the Extras to see if the images are corresponding with the product shown.
+
+    cy.get(':nth-child(1) > .extraImg > img').should('have.prop', 'src', 'https://www.stromerbike.com/bike-configurator/img/extra/b983.jpg');
+    cy.get(':nth-child(2) > .extraImg > img').should('have.prop', 'src', 'https://www.stromerbike.com/bike-configurator/img/extra/b814.jpg');
+    cy.get(':nth-child(3) > .extraImg > img').should('have.prop', 'src', 'https://www.stromerbike.com/bike-configurator/img/extra/b655.jpg');
+    cy.get(':nth-child(4) > .extraImg > img').should('have.prop', 'src', 'https://www.stromerbike.com/bike-configurator/img/extra/bbag.jpg');
+    cy.get(':nth-child(5) > .extraImg > img').should('have.prop', 'src', 'https://www.stromerbike.com/bike-configurator/img/extra/a246.jpg');
+    cy.get(':nth-child(6) > .extraImg > img').should('have.prop', 'src', 'https://www.stromerbike.com/bike-configurator/img/extra/stsh.jpg');
+    cy.get(':nth-child(7) > .extraImg > img').should('have.prop', 'src', 'https://www.stromerbike.com/bike-configurator/img/extra/stsm.jpg');
+    cy.get(':nth-child(8) > .extraImg > img').should('have.prop', 'src', 'https://www.stromerbike.com/bike-configurator/img/extra/stsl.jpg');
+    cy.get(':nth-child(9) > .extraImg > img').should('have.prop', 'src', 'https://www.stromerbike.com/bike-configurator/img/extra/pice.jpg');
+    cy.get(':nth-child(10) > .extraImg > img').should('have.prop', 'src', 'https://www.stromerbike.com/bike-configurator/img/extra/pist.jpg');
+    cy.get(':nth-child(11) > .extraImg > img').should('have.prop', 'src', 'https://www.stromerbike.com/bike-configurator/img/extra/piwt.jpg');
+    cy.get(':nth-child(12) > .extraImg > img').should('have.prop', 'src', 'https://www.stromerbike.com/bike-configurator/img/extra/pigt.jpg');
+    cy.get(':nth-child(13) > .extraImg > img').should('have.prop', 'src', 'https://www.stromerbike.com/bike-configurator/img/extra/steg.jpg');
+    cy.get(':nth-child(14) > .extraImg > img').should('have.prop', 'src', 'https://www.stromerbike.com/bike-configurator/img/extra/an20.jpg');
+    cy.get(':nth-child(15) > .extraImg > img').should('have.prop', 'src', 'https://www.stromerbike.com/bike-configurator/img/extra/an40.jpg');
+    cy.get(':nth-child(16) > .extraImg > img').should('have.prop', 'src', 'https://www.stromerbike.com/bike-configurator/img/extra/be13.jpg');
+    cy.get(':nth-child(17) > .extraImg > img').should('have.prop', 'src', 'https://www.stromerbike.com/bike-configurator/img/extra/be16.jpg');
+
+
+    cy.get('.extrasInvoice > .configuratorButton')
+      .contains('Next step')
+      .should('be.visible')
+      .click();
+  });
+});
+  
